4c039f86d2a0faf1eeac54a996b4bb0e59e52c8c
gabriel.palma.oliveira@hotmail.com <gabriel.palma.oliveira@hotmail.com>
2026-09-05 03:06:18 +0000

    hello world

---
+++ 1_1_first_write.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
+"""
+Databricks creates the SparkSession by default.
+"""
+print(spark)
+# <pyspark.sql.session.SparkSession object at 0x7f1aa82b5f70>
+
+# COMMAND ----------
+
+"""
+Spark context requires a SparkSession and in Databricks requires also dedicated access mode isolation.
+"""
+print(spark.sparkContext)
+# <SparkContext master=local[*, 4] appName=Databricks Shell>
+
+# COMMAND ----------
+
+"""
+You can create a new spark session If you want to customize session params:
+ie. spark.conf.set(...)
+"""
+spark_new = spark.newSession()
+print(spark_new)
+# <pyspark.sql.session.SparkSession object at 0x7f1a28ed3830>
